7ba6c6e4ff9a6c106e98f2d176908aaa9f2716ac
edsonJiao <1398628754@qq.com>
2019-11-15 20:50:57 +0800

    框架搭建完成

---
+++ src/main/java/com/mingshi/MingshiApplication.java
@@ -0,0 +1,15 @@
+package com.mingshi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class MingshiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MingshiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mingshi/MingshiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mingshi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MingshiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
